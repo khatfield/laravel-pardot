63ebe5a33cb4422c0c6bd8c2d18ba0586bb02fd6
soWizardly <webwizardma@gmail.com>
2020-12-08 05:07:46 -0500

    adds support for salesforce oauth and better exception handling

--- src/Pardot.php
+++ src/Pardot.php
@@ -16,12 +16,21 @@
      */
     public function connect($config, $connection = null)
     {
-      $connection = $connection ?? $config->get("pardot.default");
-      $email      = $config->get("pardot.connections.{$connection}.email");
-      $password   = $config->get("pardot.connections.{$connection}.password");
-      $user_key   = $config->get("pardot.connections.{$connection}.user_key");
+      $connection              = $connection ?? $config->get("pardot.default");
+      $email                   = $config->get("pardot.connections.{$connection}.email");
+      $password                = $config->get("pardot.connections.{$connection}.password");
+      $user_key                = $config->get("pardot.connections.{$connection}.user_key");
+      $user_api_security_token = $config->get("pardot.connections.{$connection}.user_api_security_token");
+      $auth_type               = $config->get("pardot.connections.{$connection}.auth_type");
+      $business_unit_id        = $config->get("pardot.connections.{$connection}.business_unit_id");
+      $consumer_key            = $config->get("pardot.connections.{$connection}.consumer_key");
+      $consumer_secret         = $config->get("pardot.connections.{$connection}.consumer_secret");
+      $exception_handler       = $config->get("pardot.exception_handler");
 
-      $this->client = new PardotApi($email, $password, $user_key);
+      $this->client = new PardotApi(
+        $auth_type, $email, $password, $user_key, $user_api_security_token,
+        $business_unit_id, $consumer_key, $consumer_secret, $exception_handler
+      );
 
       if($config->get("pardot.debug") == true) {
         $this->client->setDebug(true);
--- src/config/pardot.php
+++ src/config/pardot.php
@@ -12,7 +12,7 @@
     |
     */
 
-    'default' => env('PARDOT_DEFAULT_CONNECTION', 'default'),
+    'default' => env('PARDOT_DEFAULT_CONNECTION', 'pardot'),
 
     /*
     |--------------------------------------------------------------------------
@@ -22,25 +22,51 @@
     | In debug mode, pardot will throw exceptions for many queries,
     | which would otherwise return null. This can be useful for debugging,
     | but difficult to work with in many scenarios.
-    | e.x., 'getProspect' by email will return either null or an exception if
-    | no Prospect has the given email.
     |
     */
     'debug' => env('PARDOT_DEBUG_MODE', false),
 
     /*
     |--------------------------------------------------------------------------
+    | Pardot Exception Handler
+    |--------------------------------------------------------------------------
+    |
+    | In debug mode by default, 'getProspect' by email will return an exception if
+    | no Prospect has the given email. For this and other reasons, it may be useful to write
+    | your own exception handler. You can specify a class to use below.
+    |
+    */
+    'exception_handler' => env('PARDOT_EXCEPTION_HANDLER', \CyberDuck\Pardot\PardotExceptionHandler::class),
+
+    /*
+    |--------------------------------------------------------------------------
     | Pardot Connections
     |--------------------------------------------------------------------------
     |
-    | Here are each of the pardot connections setup for your application.
+    | Two examples of connections to pardot are displayed below. The "pardot" connection
+    | uses the soon to be phased out (feb '21) standard pardot method,
+    | while "sfoauth" uses the new Salesforce OAuth connection method.
+    | It is highly recommended that you use the OAuth method.
     |
     */
     'connections' => [
-      'default' => [
-        'email'    => env('PARDOT_EMAIL'),
-        'password' => env('PARDOT_PASSWORD'),
-        'user_key' => env('PARDOT_USER_KEY')
+      'pardot' => [
+        'auth_type'               => env('PARDOT_AUTH_TYPE', 'PARDOT'),
+        'email'                   => env('PARDOT_EMAIL'),
+        'password'                => env('PARDOT_PASSWORD'),
+        'user_key'                => env('PARDOT_USER_KEY'),
       ],
+      'sfoauth' => [
+        'auth_type'               => env('PARDOT_AUTH_TYPE', 'OAUTH'),
+        'email'                   => env('PARDOT_EMAIL'),
+        'password'                => env('PARDOT_PASSWORD'),
+        'user_api_security_token' => env('PARDOT_USER_API_SECURITY_TOKEN'),
+        'business_unit_id'        => env('PARDOT_BUSINESS_UNIT_ID'),
+        'consumer_key'            => env('PARDOT_CONSUMER_KEY'),
+        'consumer_secret'         => env('PARDOT_CONSUMER_SECRET')
+      ],
+
+      //...
+
     ],
   ];
